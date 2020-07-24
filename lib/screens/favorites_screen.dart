6789f8333651a7926435ea8519d978aaf705475a
kuntal305 <53057895+kuntal305@users.noreply.github.com>
2020-07-25 01:39:02 +0530

    Initial Commit

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:mealsapp/models/meal.dart';
+import 'package:mealsapp/widgets/meal_item.dart';
+
+class FavoritiesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritiesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no Favorites yet - start adding some'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            complexity: favoriteMeals[index].complexity,
+            affordability: favoriteMeals[index].affordability,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
